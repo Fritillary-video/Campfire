c6160a88ba92cbdb5b73510da608f5b3374e0c3e
whitney-to <whitney012923@gmail.com>
2023-05-05 20:06:14 -0400

    temp

--- backend/src/main/java/com/campfire/app/Campfire/Service/VideoService.java
+++ backend/src/main/java/com/campfire/app/Campfire/Service/VideoService.java
@@ -2,6 +2,7 @@ package com.campfire.app.Campfire.Service;
 
 import com.campfire.app.Campfire.Model.Video;
 import com.campfire.app.Campfire.Repository.VideoRepository;
+import com.campfire.app.Campfire.dto.VideoDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,5 +20,23 @@ public class VideoService {
         video.setVideoUrl(videoUrl);
 
         videoRepository.save(video);
+    }
+
+    public VideoDto editVideo(VideoDto videoDto) {
+        // find video by id
+        Video savedVideo = videoRepository.findById(videoDto.getId())
+                .orElseThrow(()-> new IllegalArgumentException("Cannot find video by id - "+ videoDto.getId()));
+
+        // map dto to entity
+        savedVideo.setTitle(videoDto.getTitle());
+        savedVideo.setVideoStatus(videoDto.getVideoStatus());
+//        savedVideo.setVideoUrl(videoDto.getVideoUrl());
+        savedVideo.setThumbnailUrl(videoDto.getThumbnailUrl());
+        savedVideo.setTags(videoDto.getTags());
+        savedVideo.setDescription(videoDto.getDescription());
+
+        // save vid to db
+        videoRepository.save(savedVideo);
+        return videoDto;
     }
 }
