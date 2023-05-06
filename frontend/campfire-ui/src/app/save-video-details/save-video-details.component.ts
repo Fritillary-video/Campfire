5ae9f81a7bba89bb87ccca14531e9522d1fedbf1
adams <stilwelladam8@gmail.com>
2023-05-06 12:55:27 -0400

    Added chips, and video compatibility.

--- frontend/campfire-ui/src/app/save-video-details/save-video-details.component.ts
+++ frontend/campfire-ui/src/app/save-video-details/save-video-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormControl} from "@angular/forms";
+import { MatChipEditedEvent, MatChipInputEvent } from '@angular/material/chips';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-save-video-details',
@@ -7,20 +9,61 @@ import {FormGroup, FormControl} from "@angular/forms";
   styleUrls: ['./save-video-details.component.css']
 })
 export class SaveVideoDetailsComponent implements OnInit {
-
   saveVideoDetailsForm: FormGroup;
   title: FormControl = new FormControl('');
   description: FormControl = new FormControl('');
   videoStatus: FormControl = new FormControl('');
+
+  addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  tags: string[] = [];
+
   constructor(){
-  this.saveVideoDetailsForm = new FormGroup({
-    title: this.title,
-    description: this.description,
-    videoStatus: this.videoStatus,
-  })
+    this.saveVideoDetailsForm = new FormGroup({
+      title: this.title,
+      description: this.description,
+      videoStatus: this.videoStatus,
+    })
+  
+    }
+  
+    ngOnInit(): void {
+    }
 
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value && !this.tags.includes(value)) {
+      this.tags.push(value);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
   }
 
-  ngOnInit(): void {
+  remove(tag: string): void {
+    const index = this.tags.indexOf(tag);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
   }
+
+  edit(tag: string, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+
+    // Remove fruit if it no longer has a name
+    if (!value) {
+      this.remove(tag);
+      return;
+    }
+
+    // Edit existing fruit
+    const index = this.tags.indexOf(tag);
+    if (index >= 0) {
+      this.tags[index] = value;
+    }
+  }
+
 }
