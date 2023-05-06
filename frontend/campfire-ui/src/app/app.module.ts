82a165a13538143109cedf892b803f0757ca5f69
adams <stilwelladam8@gmail.com>
2023-05-06 17:30:33 -0400

    Finished login identification.

--- frontend/campfire-ui/src/app/app.module.ts
+++ frontend/campfire-ui/src/app/app.module.ts
@@ -25,6 +25,7 @@ import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
 import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { VideoPlayerComponent } from './video-player/video-player.component';
+import { AuthConfigModule } from './auth/auth-config.module';
 
 
 @NgModule({
@@ -57,6 +58,7 @@ import { VideoPlayerComponent } from './video-player/video-player.component';
     VgOverlayPlayModule,
     VgBufferingModule,
     MatSnackBarModule,
+    AuthConfigModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
