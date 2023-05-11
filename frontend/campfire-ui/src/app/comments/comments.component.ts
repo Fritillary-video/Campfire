1e171664db88a2ae135b46da21ac26f9cbe0d137
whitney-to <whitney012923@gmail.com>
2023-05-11 14:35:29 -0400

    added comment service

--- frontend/campfire-ui/src/app/comments/comments.component.ts
+++ frontend/campfire-ui/src/app/comments/comments.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-comments',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./comments.component.css']
 })
 export class CommentsComponent {
+
+  commentsForm : FormGroup;
+
+  constructor(private userService : UserService){
+    this.commentsForm = new FormGroup({
+      comment: new FormControl('comment'),
+    });
+  }
+
+  ngOnInit() : void {
+
+  }
+
+  postComment(){
+    const comment = this.commentsForm.get('comment')?.value;
+    const commentDTO = {
+      "commentText" : comment,
+      "authorId" : this.userService.getUserId(),
+    }
+  }
 
 }
