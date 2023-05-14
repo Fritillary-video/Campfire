4c09495fbc11135c91953f0635b5617c8fe02ad9
Seth <seth@Zipcoders-MacBook-Pro.local>
2023-05-14 14:43:14 -0400

    unbusted the redirect

--- frontend/campfire-ui/src/app/user.service.ts
+++ frontend/campfire-ui/src/app/user.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { VideoDto } from './video-dto';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class UserService {
 
   private userId: string = '';
 
-  constructor(private httpClient: HttpClient, private oss: OidcSecurityService) {
+  constructor(private httpClient: HttpClient, private oss: OidcSecurityService, private router: Router) {
   }
 
   subscribeToUser(userId: string): Observable<boolean> {
@@ -43,12 +44,13 @@ export class UserService {
   registerUser() {
     this.oss.userData$.subscribe(userData => {
       this.oss.getAccessToken().subscribe(access => {
-        console.log("data " + JSON.stringify(access));
+        //console.log("data " + JSON.stringify(access));
         if (access !== null) {
           this.httpClient.get("http://localhost:8080/api/user/register", { responseType: "text" })
             .subscribe(data => {
-              console.log("got data")
+              //console.log("got data")
               this.userId = data;
+               this.router.navigateByUrl('/featured');
             });
         }
       });
