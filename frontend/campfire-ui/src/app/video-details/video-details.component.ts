a414327dc3a0f544db957abf66fd808bbe4560cc
timparka <pimtarka@gmail.com>
2023-05-15 09:39:19 -0400

    got the subscribe button to work!!!

--- frontend/campfire-ui/src/app/video-details/video-details.component.ts
+++ frontend/campfire-ui/src/app/video-details/video-details.component.ts
@@ -1,16 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { VideoService } from '../video.service';
 import { UserService } from '../user.service';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
-//import { UserInfoDTO } from '../user-into-dto';
 
 @Component({
   selector: 'app-video-details',
   templateUrl: './video-details.component.html',
   styleUrls: ['./video-details.component.css']
 })
-export class VideoDetailsComponent {
+export class VideoDetailsComponent implements OnInit {
   videoId!: string;
   videoUrl!: string;
   videoAvailable: boolean = false;
@@ -29,54 +28,46 @@ export class VideoDetailsComponent {
   subscribers!: number;
 
 
- constructor(private activatedRoute: ActivatedRoute, private videoService: VideoService,
-     private userService: UserService, private oidcSecurityService: OidcSecurityService) {
-     this.videoId = this.activatedRoute.snapshot.params['videoId'];
+  constructor(private activatedRoute: ActivatedRoute, private videoService: VideoService,
+    private userService: UserService, private oidcSecurityService: OidcSecurityService) {
+    this.videoId = this.activatedRoute.snapshot.params['videoId'];
+  }
 
-     this.videoService.getVideo(this.videoId).subscribe(data => {
-       this.videoUrl = data.videoUrl;
-       this.videoAvailable = true;
-       this.videoTitle = data.title;
-       this.videoDescription = data.description;
-       this.tags = data.tags;
-       this.likeCount = data.likeCount;
-       this.dislikeCount = data.dislikeCount;
-       this.viewCount = data.viewCount;
-       this.datePosted = data.datePosted;
-       this.uploaderId = data.userId;
-       console.log(data.userId);
+  ngOnInit(): void {
+    this.oidcSecurityService.isAuthenticated$.subscribe(({isAuthenticated}) => {
+      this.isAuthenticated = isAuthenticated;
 
-       // Call getUserProfile() after uploaderId is set
-       this.userService.getUserProfile(this.uploaderId).subscribe(profileData => {
-         this.accountName = profileData.email;
-         this.subscribers = profileData.subscribers.size;
-         this.userService.isSubscribed(this.userService.getUserId()).subscribe(isSubscribed => {
-                  this.showSubscribeButton = isSubscribed;
-                  this.showUnsubscribeButton = !isSubscribed;
-                  console.log(isSubscribed);
-                });
-       });
+      this.videoService.getVideo(this.videoId).subscribe(data => {
+        this.videoUrl = data.videoUrl;
+        this.videoAvailable = true;
+        this.videoTitle = data.title;
+        this.videoDescription = data.description;
+        this.tags = data.tags;
+        this.likeCount = data.likeCount;
+        this.dislikeCount = data.dislikeCount;
+        this.viewCount = data.viewCount;
+        this.datePosted = data.datePosted;
+        this.uploaderId = data.userId;
 
-       console.log("current userId:" + data.userId);
-     });
- }
+        this.userService.getUserProfile(this.uploaderId).subscribe(profileData => {
+          this.accountName = profileData.email;
+          this.subscribers = profileData.subscribers.size;
 
+          if (this.isAuthenticated) {
+            this.checkSubscriptionStatus();
+          }
+        });
+      });
+    });
+  }
 
+  checkSubscriptionStatus(): void {
+    this.userService.isSubscribed(this.uploaderId).subscribe(isSubscribed => {
+      this.showSubscribeButton = !isSubscribed;
+      this.showUnsubscribeButton = isSubscribed;
+    });
+  }
 
-   ngOnInit(): void {
-     this.oidcSecurityService.isAuthenticated$.subscribe(({isAuthenticated}) => {
-       this.isAuthenticated = isAuthenticated;
-
-
-       // If the user is authenticated, check if they are subscribed to the video uploader
-       if (this.isAuthenticated) {
-         this.userService.isSubscribed(this.uploaderId).subscribe(isSubscribed => {
-           this.showSubscribeButton = !isSubscribed;
-           this.showUnsubscribeButton = isSubscribed;
-         });
-       }
-     });
-   }
 
   likeVideo() {
     this.videoService.likeVideo(this.videoId).subscribe(data => {
@@ -93,21 +84,18 @@ export class VideoDetailsComponent {
   }
 
   subscribeToUser() {
-    const currentUserId = this.userService.getUserId(); // Get the current user's ID
-    const uploaderId = this.uploaderId; // Get the uploader's ID
+    const currentUserId = this.userService.getUserId();
+    const uploaderId = this.uploaderId;
     this.userService.subscribeToUser(currentUserId, uploaderId).subscribe(data => {
-      this.showSubscribeButton = false;
-      this.showUnsubscribeButton = true;
+      this.checkSubscriptionStatus();
     });
   }
 
   unsubscribeToUser() {
     const currentUserId = this.userService.getUserId();
-    const uploaderId = this.uploaderId; // Get the uploader's ID
+    const uploaderId = this.uploaderId;
     this.userService.unsubscribeToUser(currentUserId, uploaderId).subscribe(data => {
-      this.showSubscribeButton = true;
-      this.showUnsubscribeButton = false;
+      this.checkSubscriptionStatus();
     });
   }
-
 }
